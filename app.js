61f91a3fed2a225bfdab66556b3e26346efb5afe
Alasgar M <mahmudov.alasgar@gmail.com>
2024-01-04 02:30:51 +0400

    products services

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
 // show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
+// navBtn.addEventListener("click", () => {
+//   navbar.classList.add("showNav");
+// });
 // close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
+// navClose.addEventListener("click", () => {
+//   navbar.classList.remove("showNav");
+// });
 //setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+// const date = (document.getElementById(
+//   "date"
+// ).innerHTML = new Date().getFullYear());
+
+// Scroll to the bottom of the page
+function scrollToBottom() {
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth', // You can change this to 'auto' for an instant scroll
+  });
+}
+
+// Call the function to scroll to the bottom
+scrollToBottom();
